c7ab25fd9b28ebd6a1e2d80609c7fab07a32d309
Nick Faber <nfaber@sandiego.edu>
2021-03-11 19:05:40 -0800

    got chart with data points as bubbles

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -15,7 +15,7 @@ var height = svgHeight - margin.top - margin.bottom;
 //AND SHIFT THE LATTER BY LEFT AND TOP MARGINS
 
 var svg = d3
-  .select(".chart")
+  .select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -24,6 +24,19 @@ var svg = d3
 
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// INITIAL PARAMS 
+
+function xScale(data, chosenXAxis) {
+    
+    var xLinearScale = d3.scaleLinear()
+        .domain([d3.min(data, d => d[chosenXAxis]) * 0.75,
+            d3.max(data, d => d[chosenXAxis]) * 1.25
+        ])
+        // .range([0. width]);
+
+    return xLinearScale;
+};
 
 // GET DATA
 
@@ -37,6 +50,47 @@ d3.csv("assets/data/data.csv").then(function (data, err) {
     data.healthcare = +data.healthcare;
     data.obesity = +data.obesity;
   });
+
+  // X LINEAR SCALE
+
+  var xLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.poverty)])
+        .range([height, 0]);
+
+    // CREATE SCALES
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.healthcare)])
+        .range([height, 0]);
+  
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+  // append x axis
+  var xAxis = chartGroup.append("g")
+    .classed("x-axis", true)
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+  // append y axis
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  // append initial circles
+  var circlesGroup = chartGroup.selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", 20)
+    .attr("fill", "red")
+    .attr("opacity", ".3");
+
+     // Create group for two x-axis labels
+    var labelsGroup = chartGroup.append("g")
+    .attr("transform", `translate(${width / 2}, ${height + 20})`);
+
+  
 
 //   };
 
